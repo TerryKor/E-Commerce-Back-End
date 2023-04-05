d7212be75ab0d9a0979b6818d56b92cf1cc2010a
Terry <misterfreemann@gmail.com>
2023-04-05 14:31:20 -0400

    fixed Delete method in products, now able to delete product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -45,11 +45,7 @@ router.post("/", (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  // try{
-  //   const productData = await Product.create({
-
-  //   })
-  // }
+  
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -119,7 +115,7 @@ router.delete("/:id", async (req, res) => {
   try {
     const productData = await Product.destroy({
       where: {
-        id: req.body.id,
+        id: req.params.id,
       },
     });
     if (!productData) {
